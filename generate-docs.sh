7cfaac5e1e50d76ba746588debd8569ab153340c
Tomasz Michalak <8047609+tomaszmichalak@users.noreply.github.com>
2024-01-23 10:45:54 +0100

    DXP-450 Restructure repo to enable GH Pages release

--- generate-docs.sh
+++ generate-docs.sh
@@ -14,4 +14,5 @@
 
 #!/usr/bin/env bash
 
-docker run --rm --volume "$(pwd):/helm-docs" -u $(id -u) jnorwood/helm-docs:latest
+docker run --rm --volume "$(pwd)/streamx:/helm-docs" -u $(id -u) jnorwood/helm-docs:latest
+mv streamx/README.md .
